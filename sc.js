a69e9489bfbb383e80c9cd92386ddce0c8ffb740
Your Name <you@example.com>
2023-07-31 21:51:04 +0530

    page2 text animation

--- sc.js
+++ sc.js
@@ -17,3 +17,27 @@ t.from("#page1 svg",{
     delay:-0.1,
     duration:0.2,
 })
+
+var data=document.querySelectorAll("#page2 #textcon h2")
+data.forEach(function(e){
+    var tdata=e.textContent
+    var stext=tdata.split("")
+var clutter=""
+stext.forEach(function(elem){
+    clutter+=`<span>${elem}</span>`
+})
+e.innerHTML=clutter
+})
+gsap.to("#page2 #textcon h2 span",{
+    color:"#E3E3C4",
+    stagger: 0.2,
+    scrollTrigger:{
+        trigger:"#page2 #textcon h2 span",
+        scroller:"body",
+        // makers:true,
+        start:"top 50%",
+        end:"top -20%",
+        scrub:3
+    }
+})
+
